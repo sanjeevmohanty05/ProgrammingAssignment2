fc567c972068b6953b9de58b431dc93f73036935
Sanjeev <sanjeevmohanty05@gmail.com>
2018-12-06 22:34:53 +0530

    Rename cachematrix.R to cachematrix.r

--- cachematrix.r
+++ cachematrix.r
@@ -19,17 +19,6 @@ makeCacheMatrix <- function(x = matrix()) {
 from makeCacheMatrix(matrix) has any value in it or not.In case inverse matrix from makeCacheMatrix((matrix) is empty,
 it gets the original matrix data from and set the invertible  matrix by using the solve function. 
 
-
- 
-# In case inverse matrix from makeCacheMatrix((matrix) has some value in it (always works 
-
-
- 
-#after running the code 1st time), it returns a message  "Getting Cached Invertible Matrix"  
-
-
- 
-#and the cached object 
 cacheSolve <- function(x, ...) {
   inv <- x$getInverse()
   if(!is.null(inv)){
